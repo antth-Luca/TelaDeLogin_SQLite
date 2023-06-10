5cb0a40736021fc05f6af5ca853cf65b38e7584e
Luca Anthony <lucamafort05@gmail.com>
2023-06-09 22:39:33 -0300

    Início_parte2

---
+++ Parte 2/main.py
@@ -0,0 +1,28 @@
+from PyQt5 import uic, QtWidgets
+
+def enviando():
+    primTela.label_2.setText('')
+    user = primTela.lineEdit.text()
+    password = primTela.lineEdit_3.text()
+    if user == 'admin_Luca' and password == '12345678':
+        primTela.close()
+        segTela.show()
+    else:
+        primTela.label_2.setText('Usuário ou senha errado(s). Tente novamente')
+
+
+def saindo():
+    segTela.close()
+    primTela.show()
+
+
+app = QtWidgets.QApplication([])
+primTela = uic.loadUi('primeiraJanela.ui')
+segTela = uic.loadUi('segundaJanela.ui')
+primTela.lineEdit_3.setEchoMode(QtWidgets.QLineEdit.Password)
+
+primTela.pushButton.clicked.connect(enviando)
+segTela.pushButton.clicked.connect(saindo)
+
+primTela.show()
+app.exec()
